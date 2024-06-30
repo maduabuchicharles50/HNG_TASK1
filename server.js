7a8d86eec470a4e769cb24343df235bdbd00d898
Charles <maduabuchicharles50@gmail.com>
2024-06-30 22:12:40 +0100

    Feat: web server setup

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+const port = 4000;
+
+app.get("/api/", (req, res) => {
+  res.send("Hello, Mark!, the temperature is 11 degrees Celcius in New York");
+});
+
+app.listen(port, () => {
+  console.log(`server running on port ${port}...`);
+});
